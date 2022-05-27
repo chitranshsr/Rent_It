b321c0c072c349f12b97794aef2e744a11815bb5
Chitransh Srivastava <100120691+chitranshsr@users.noreply.github.com>
2022-05-27 20:56:19 +0530

    Added Files
    Products Page Frontend

---
+++ Products.js
@@ -0,0 +1,245 @@
+import React from 'react'
+// import productjs from './ProductsJs';
+import '../services/product.css'
+function Products() {
+  return (
+    <>
+      
+ 
+
+<div class="small-container">
+  <div class="row row-2">
+    <h2>All Products</h2>
+  
+  </div>
+  <div class="row">
+    <div class="col-4">
+      <img src="https://i.ibb.co/47Sk9QL/product-1.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/b7ZVzYr/product-2.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/KsMVr26/product-3.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star-half-alt"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/0cMfpcr/product-4.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-4">
+      <img src="https://i.ibb.co/47Sk9QL/product-1.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/b7ZVzYr/product-2.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/KsMVr26/product-3.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star-half-alt"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/0cMfpcr/product-4.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-4">
+      <img src="https://i.ibb.co/bQ5t8bR/product-5.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/vVpTyBD/product-6.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/hR5FGwH/product-7.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star-half-alt"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/QfCgdXZ/product-8.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-4">
+      <img src="https://i.ibb.co/nw5xZwk/product-9.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/9HCsmjf/product-10.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/JQ2MBHR/product-11.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star-half-alt"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/nRZMs6Y/product-12.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>₹500.00</p>
+    </div>
+  </div>
+
+  
+</div>
+
+
+
+
+
+    </>
+
+
+    
+  )
+}
+
+export default Products
