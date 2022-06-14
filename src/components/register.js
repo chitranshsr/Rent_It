73409316dac4c835af734099d1b87e2820a6ee65
chitranshsr <chitransh.srivastava@splashlearn.com>
2022-06-14 19:00:16 +0530

    Added CRUD  Functionalities

--- src/components/register.js
+++ src/components/register.js
@@ -184,18 +184,18 @@ export default class Register extends Component {
                   />
                 </div>
 
-                <div className="form-group">
+                <div className="form-group" >
                   <button className="btn btn-primary btn-block">Sign Up</button>
                 </div>
               </div>
             )}
 
             {this.state.message && (
-              <div className="form-group">
+              <div className="form-group" style={{marginTop: "4rem" }}>
                 <div
                   className={
                     this.state.successful
-                      ? "alert alert-success"
+                      ? "alert alert-success "
                       : "alert alert-danger"
                   }
                   role="alert"
